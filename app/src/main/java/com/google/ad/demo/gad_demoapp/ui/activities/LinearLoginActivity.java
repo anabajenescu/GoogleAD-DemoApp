ddab0b093660438bf4b88df1842e307ad5aa3740
Madalin Gageanu <madalin.gageanu@gmail.com>
2018-11-26 14:46:34 +0200

    Added login button handling.

--- app/src/main/java/com/google/ad/demo/gad_demoapp/ui/activities/LinearLoginActivity.java
+++ app/src/main/java/com/google/ad/demo/gad_demoapp/ui/activities/LinearLoginActivity.java
@@ -2,6 +2,9 @@ package com.google.ad.demo.gad_demoapp.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.ad.demo.gad_demoapp.R;
 
@@ -11,5 +14,18 @@ public class LinearLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_login);
+    }
+
+    public void doLogin(View view)
+    {
+
+        EditText editUsername = findViewById(R.id.actLinearLayout_editUser);
+        EditText editPassword = findViewById(R.id.actLinearLayout_editPass);
+
+        /* password validation here */
+        if (true) {
+            Toast.makeText(this, R.string.actLinearLogin_text_login_success, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
